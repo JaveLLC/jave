9f382e29159892c889f4513b24cba59624b87547
Charles Corbett <nafredy@gmail.com>
2018-10-13 00:04:56 -0700

    Loads files and packs them into a shit struct

--- main.go
+++ main.go
@@ -4,6 +4,28 @@
 
 package main
 
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/OrcaLLC/jave/src/lexer"
+)
+
 func main() {
+	// Fetch arguments to get the filename
+	runArgs := os.Args[1:]
+	filePath := runArgs[0]
+
+	// Jave can only run one file at a time, dingus
+	if len(runArgs) > 1 {
+		log.Printf("Additional arguments ignored. Using: [%s] but found %v\n", filePath, runArgs)
+	}
+
 	// Load what we need then load either the file handler or input getter
+	thing, err := lexer.NewJaveFile(filePath)
+	if err != nil {
+		log.Fatalf("[JAVE FAIL]: %v\n", err)
+	}
+	fmt.Printf("%v", thing)
 }
--- src/lexer/javefile.go
+++ src/lexer/javefile.go
@@ -1,1 +1,31 @@
 package lexer
+
+import (
+	"io/ioutil"
+	"path/filepath"
+)
+
+// JaveFile defines incoming jave source
+type JaveFile struct {
+	Path     string
+	Name     string
+	Contents []rune
+	NewLines []int
+}
+
+// NewJaveFile ingests jave source file
+func NewJaveFile(path string) (*JaveFile, error) {
+	// Get the filename
+	filename := filepath.Base(path)
+
+	jf := &JaveFile{Name: filename, Path: path}
+	jf.NewLines = append(jf.NewLines, -1)
+
+	contents, err := ioutil.ReadFile(jf.Path)
+	if err != nil {
+		return nil, err
+	}
+
+	jf.Contents = []rune(string(contents))
+	return jf, nil
+}
